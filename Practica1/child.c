98f3ffaa4ad8251c7705a999ac84121fe97e7ac0
Marjxg <rmarjoriee71@gmail.com>
2024-02-19 23:50:33 +0000

    Agreando funcionalidad al child

--- Practica1/child.c
+++ Practica1/child.c
@@ -4,8 +4,35 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 
 void main(int argc, char *argv[]){
-    printf("\nSoy el proceso hijo %s\n", argv[2]);
-    sleep(3);
+    srand(time(NULL) + getpid());
+    int randomFunc = rand() % 3 + 1;
+    int randomTime = rand() % 3 + 1;
+
+    if (randomFunc == 1){
+        //Escribir
+        printf("Escribir -> %s \n", argv[2]);
+        int fileDescript = open("practica1.txt", O_RDONLY);
+        write(fileDescript, "abcdefgh", strlen("abcdefgh"));
+        sleep(randomTime);
+    } else if (randomFunc == 2){
+        //Leer
+        printf("Leer -> %s \n", argv[2]);
+        int fileDescript = open("practica1.txt", O_RDONLY);
+        char buf[8];
+        size_t nBytes;
+        read(fileDescript, buf, nBytes); 
+        sleep(randomTime);
+        
+    } else if (randomFunc == 3){
+        //Seek
+        printf("Seek -> %s \n", argv[2]);
+        int fileDescript = open("practica1.txt", O_RDONLY);
+        write(fileDescript, "12345678", strlen("12345678"));
+        lseek(fileDescript, 0, SEEK_SET);
+        sleep(randomTime);
+    }
 }
--- Practica1/parent.c
+++ Practica1/parent.c
@@ -4,10 +4,17 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-void main()
-{
+void main(){
+    //Creacion archivo
+    int fileDescript = open("practica1.txt", O_RDWR | O_CREAT, 0777);
+    close(fileDescript);
+
+    //Creacion primer proceso hijo
     pid_t child1 = fork();
+    int pid1 = child1;
+
     if (child1 == -1){
         perror("fork");
         exit(1);
@@ -23,6 +30,13 @@ void main()
         execv("/home/mint/Documents/SO2_202000560/Practica1/child.bin", argPtr);
     } else {
         pid_t child2 = fork();
+        int pid2 = child2;
+
+        printf("PIDS %d %d\n", pid1, pid2);
+            char command[100];
+            sprintf(command, "%s %d %d %s", "sudo stap trace.stp", pid1, pid2, " > syscalls.log");
+            printf("%s \n", command);
+            system(command);
 
         if (child2 == -1){
             perror("fork");
@@ -30,16 +44,15 @@ void main()
         }
 
         if (child2 == 0){
+
             char *argPtr[4];
             argPtr[0] = "child.c";
-            argPtr[1] = "Hola";
+            argPtr[1] = "hola";
             argPtr[2] = "Soy el proceso hijo 2";
             argPtr[3] = NULL;
 
-        execv("/home/mint/Documents/SO2_202000560/Practica1/child.bin", argPtr);
-        } 
-
-        printf("Soy el proceso Padre");
+            execv("/home/mint/Documents/SO2_202000560/Practica1/child.bin", argPtr);
+        } else {
 
             int status;
             wait(&status);
@@ -47,7 +60,19 @@ void main()
             if(WIFEXITED(status)){
                 printf("\nLos procesos hijos terminaron con estado = %d\n", WIFEXITED(status));
             } else {
-                printf("Ocurrio un error al terminar el proceso hijo 1");
+                printf("Ocurrio un error al terminar el proceso hijo 1\n");
+            }
+        }
+
+        printf("Soy el proceso Padre\n");
+
+            int status;
+            wait(&status);
+
+            if(WIFEXITED(status)){
+                printf("\nLos procesos hijos terminaron con estado = %d\n", WIFEXITED(status));
+            } else {
+                printf("Ocurrio un error al terminar el proceso hijo 1\n");
             }
 
             printf("Terminando proceso padre \n");
